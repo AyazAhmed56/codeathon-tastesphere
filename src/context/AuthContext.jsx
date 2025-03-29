80859465bfdbfb32e7f872ee46b68e339cd2098d
Ayaz Ahmed Siddiqui <ayazsiddiqui0586@gmail.com>
2025-03-30 02:00:53 +0530

    Updated project files

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,29 @@
+import { createContext, useContext, useState } from "react";
+import React from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const login = (email, password) => {
+    const userData = { email };
+    localStorage.setItem("user", JSON.stringify(userData));
+    setUser(userData);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("user");
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
